45307f3f3c5d58880292a36e36783759ca36ef5a
Andrey Chorniy <creatuseandr@icloud.com>
2023-12-10 22:27:52 +0200

    set up radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@radix-ui/themes';
 
 const IssuePage = () => {
   return (
-    <div>IssuePage</div>
-  )
-}
+    <div>
+      <Button>
+        New Issue
+      </Button>
+    </div>
+  );
+};
 
-export default IssuePage
+export default IssuePage;
