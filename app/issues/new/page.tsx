d406452c6e7d85854338b88a5123b493abee060c
Andrey Chorniy <creatuseandr@icloud.com>
2023-12-11 22:30:30 +0200

    implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -5,6 +5,7 @@ import 'easymde/dist/easymde.min.css';
 import {
   Button,
   Callout,
+  Text,
   TextField,
 } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
@@ -12,24 +13,30 @@ import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssue = () => {
   const router = useRouter();
-  const [errors, setErrors] = useState('');
+  const [error, setError] = useState('');
 
-  const { register, control, handleSubmit } =
-    useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
 
   return (
     <div className='max-w-xl'>
-      {errors && (
+      {error && (
         <Callout.Root color='red' className='mb-5'>
-          <Callout.Text>{errors}</Callout.Text>
+          <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
       <form
@@ -39,7 +46,7 @@ const NewIssue = () => {
             await axios.post('/api/issues', data);
             router.push('/issues');
           } catch (error) {
-            setErrors('An unexpected error occurred.');
+            setError('An unexpected error occurred.');
           }
         })}>
         <TextField.Root>
@@ -48,6 +55,11 @@ const NewIssue = () => {
             {...register('title')}
           />
         </TextField.Root>
+        {errors && errors.title && (
+          <Text color='red' as='p'>
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name='description'
           control={control}
@@ -58,6 +70,11 @@ const NewIssue = () => {
             />
           )}
         />
+        {errors && errors.title && (
+          <Text color='red' as='p'>
+            {errors.description?.message}
+          </Text>
+        )}
         <Button>Submit new Issue</Button>
       </form>
     </div>
---
+++ app/validationSchemas.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const createIssueSchema = z.object({
+  title: z.string().min(1, 'Title is required.').max(25),
+  description: z
+    .string()
+    .min(1, 'Description is required.'),
+});
