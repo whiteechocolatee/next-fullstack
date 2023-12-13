856b9cad6b72842dd79953a89215fc33bf622739
Andrey Chorniy <creatuseandr@icloud.com>
2023-12-13 19:52:07 +0200

    add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,7 +7,7 @@ import {
   Text,
 } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 interface Props {
   params: { id: string };
@@ -31,7 +31,9 @@ const IssuePageDetails = async ({ params }: Props) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>{issue.description}</Card>
+      <Card className='prose mt-4'>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
     </div>
   );
 };
